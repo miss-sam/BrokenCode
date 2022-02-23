0947525c8261dabca5059843f03767a2adbbf8f1
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2022-02-22 18:53:03 -0700

    Update BrokenCode.c

--- BrokenCode.c
+++ BrokenCode.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
-    char str[];
+    /*The code from here to line 13 should work. This code retrieves the time from the system, however it may be in the wrong time zone. Also, ignore the error ending with [-Wint-conversion]*/
+    
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer [80];
+
+    time (&rawtime);
+    timeinfo = localtime (&rawtime);
+    /* All errors will be from here down */
+    
+ 
+ 
+ 
+ 
+ 
+ 
+    char timeval= localtime(&(time_t){time(NULL)});
+    char str[10];
     printf("Please enter your name: ");
     scanf("%s", str);
     getchar();
-    printf("Hello%s. \n", str);
+    printf("Hello %s. \n", str);
+    
+    strftime (buffer,80,"The time is currently %I:%M%p.",timeinfo);
+    puts (buffer);
     return 0;
 }
