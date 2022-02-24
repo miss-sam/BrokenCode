0c2628ee25ade6bdcdc3cb6da67d980f46f4f431
Sam <100165896+miss-sam@users.noreply.github.com>
2022-02-23 17:13:36 -0700

    Update BrokenCode.py

--- BrokenCode.py
+++ BrokenCode.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 from datetime import date
 today = date.today()
-username=str(input('Hello User, What is your name? '))
-print('Hello, '+username+' The current date is '+today)
+username=(input('Hello User, What is your name? '))
+print('Hello,', username, ' The current date is', today)
